8dcc93897646e5c57a580ad6a165787ef02e120c
JackTheRipper2312 <54137005+JackTheRipper2312@users.noreply.github.com>
2021-01-18 13:23:58 +0100

    Spillemenu færdiggørelse

--- Avancerede del/src/advanced/MinesweeperMain.java
+++ Avancerede del/src/advanced/MinesweeperMain.java
@@ -6,70 +6,29 @@ import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.fxml.FXML;
-import javafx.scene.Node;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
-import java.io.IOException;
 
 
 public class MinesweeperMain extends Application {
 
-	public static void main(String[] args) {
-		try {
-			createGame(args);
-			launch();
-		} catch (IllegalArgumentException e) {
-			System.out.println("\nFail: " + e.getMessage());
-			System.exit(0);
-		}
-	}
+    public static void main(String[] args) {
+        launch();
+    }
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
 
-		MinesweeperGame.setMain(this);
+        primaryStage.setTitle("Minesweeper");
+        primaryStage.setResizable(false);
+        Image logo = new Image("Flag.png");
+        primaryStage.getIcons().add(logo);
 
-		primaryStage.setTitle("Minesweeper");
-		primaryStage.setResizable(false);
-		Image logo = new Image("Flag.png");
-		primaryStage.getIcons().add(logo);
+        Parent mainRoot = FXMLLoader.load(getClass().getResource("Game.fxml"));
+        Scene mainScene = new Scene(mainRoot);
 
-		Parent mainRoot = FXMLLoader.load(getClass().getResource("MainGame.fxml"));
-		Scene mainScene = new Scene(mainRoot);
+        Parent mainMenu = FXMLLoader.load(getClass().getResource("Menu.fxml"));
+        Scene mainMenuScene = new Scene(mainMenu);
 
-		Parent mainMenu = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
-		Scene mainMenuScene = new Scene(mainMenu);
-
-		primaryStage.setScene(mainMenuScene);
-		primaryStage.show();
-	}
-
-	// Validates arguments and constructs game
-	public static void createGame(String[] args) throws IllegalArgumentException {
-		int width, height, mines;
-		
-		try {
-			if (args.length != 3) {
-				throw new IllegalArgumentException("Arguments must be on format: width (4 to 100), height (4 to 100), mines (1 to area - 9).");
-			}
-
-			width = Integer.parseInt(args[0]);
-			height = Integer.parseInt(args[1]);
-			mines = Integer.parseInt(args[2]);
-			if (width < 4 || width > 100 || height < 4 || height > 100 || mines < 1 || mines > width * height - 9) {
-				throw new IllegalArgumentException("Arguments must be on format: width (4 to 100), height (4 to 100), mines (1 to area - 9).");
-			}
-			
-		} catch (NumberFormatException e) {
-			throw new IllegalArgumentException("Arguments must be integers!");
-		}
-		
-		MinesweeperGame.constructGame(width, height, mines);
-	}
-
+        primaryStage.setScene(mainMenuScene);
+        primaryStage.show();
+    }
 }
